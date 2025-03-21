3271f68cf9017ee9897a6259ab11900d1009cb89
Davi Conrado <daviconradoneto@gmail.com>
2025-03-21 02:30:30 -0300

    refactor swagger architecture

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,9 @@ import {routes} from "./routes"
 import { errorHandler } from "./middlewares/error-handler";
 import mongoose from "mongoose";
 import rateLimit from "express-rate-limit";
-import path from 'path';
 require('dotenv').config();
 
-import { generateSchema } from '@anatine/zod-openapi';
-import { loginSchemaZod, userSchemaZod } from "./validations/zod-schemas/user-schema-zod";
-import { eventSchemaZod } from "./validations/zod-schemas/event-schema-zod";
-import { ticketSchemaZod } from "./validations/zod-schemas/ticket-schema-zod";
-const swaggerJsdoc = require('swagger-jsdoc');
+import { swaggerSpec } from "./swagger.config";
 const swaggerUi = require('swagger-ui-express');
 
 const limiter = rateLimit({
@@ -20,38 +15,6 @@ const limiter = rateLimit({
   legacyHeaders: false,
   message: 'Too many requests, please try again later',
 });
-
-const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Event API',
-      version: '1.0.0',
-      description: 'API Documentation',
-    },
-    servers: [
-      { url: 'http://localhost:3333', description: 'Servidor local' },
-    ],
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
-        },
-      },
-      schemas: {
-        User: generateSchema(userSchemaZod),
-        Login: generateSchema(loginSchemaZod),
-        Ticket: generateSchema(ticketSchemaZod),
-        Event: generateSchema(eventSchemaZod)
-      }
-    },
-  },
-  apis: [path.join(__dirname, 'routes/*.ts')], 
-};
-
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 const DB_CONNECTION_URL = process.env.DB_CONNECTION_URL;
 if (!DB_CONNECTION_URL) {
---
+++ src/swagger.config.ts
@@ -0,0 +1,41 @@
+import { generateSchema } from '@anatine/zod-openapi';
+import { loginSchemaZod, userSchemaZod } from "./validations/zod-schemas/user-schema-zod";
+import { eventSchemaZod } from "./validations/zod-schemas/event-schema-zod";
+import { ticketSchemaZod } from "./validations/zod-schemas/ticket-schema-zod";
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+import path from 'path';
+
+const swaggerOptions = {
+    swaggerDefinition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Event API',
+        version: '1.0.0',
+        description: 'API Documentation',
+      },
+      servers: [
+        { url: 'http://localhost:3333', description: 'Servidor local' },
+      ],
+      components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+          },
+        },
+        schemas: {
+          User: generateSchema(userSchemaZod),
+          Login: generateSchema(loginSchemaZod),
+          Ticket: generateSchema(ticketSchemaZod),
+          Event: generateSchema(eventSchemaZod)
+        }
+      },
+    },
+    apis: [path.join(__dirname, 'routes/*.ts')], 
+  };
+  
+  const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+export {swaggerSpec}
